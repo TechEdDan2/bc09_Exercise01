99de4bc00b46f372162448e998110fec624a4124
TechEdDan2 <dg.nelson2@gmail.com>
2024-06-08 08:53:41 -0400

    Completed a few more functions

--- foreach-map-filter.js
+++ foreach-map-filter.js
@@ -136,6 +136,9 @@ Examples:
 */
 
 function extractKey(arr, key) {
+    return arr.map(function (val) {
+        return val[key];
+    })
 
 }
 
@@ -147,6 +150,10 @@ Examples:
 */
 
 function extractFullName(arr) {
+    return arr.map(function (val) {
+        // console.log(val.first + " " + val.last);
+        return val.first + " " + val.last
+    });
 
 }
 
@@ -157,7 +164,11 @@ Examples:
     filterByValue([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele", isCatOwner: true}], 'isCatOwner') // [{first: 'Tim', last:"Garcia", isCatOwner: true}, {first: 'Colt', last:"Steele", isCatOwner: true}]
 */
 
-function filterByValue(arr, key) { }
+function filterByValue(arr, key) {
+    return arr.filter(function (val) {
+        return val[key] !== undefined;
+    })
+}
 
 /*
 Write a function called find which accepts an array and a value and returns the first element in the array that has the same value as the second parameter or undefined if the value is not found in the array.
@@ -167,7 +178,9 @@ Examples:
     find([1,2,3,4,5], 10) // undefined
 */
 
-function find(arr, searchValue) { }
+function find(arr, searchValue) {
+
+}
 
 /*
 Write a function called findInObj which accepts an array of objects, a key, and some value to search for and returns the first found value in the array.
